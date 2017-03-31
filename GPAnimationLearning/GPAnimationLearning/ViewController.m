bf84bd207b73658e05ce0788092798d0eaceb554
DanDan <821403986@qq.com>
2017-03-31 10:15:56 +0800

    哈哈

--- GPAnimationLearning/GPAnimationLearning/ViewController.m
+++ GPAnimationLearning/GPAnimationLearning/ViewController.m
@@ -8,16 +8,77 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *rootTableView;
+@property (nonatomic, strong) NSArray *titleArray;
+@property (nonatomic, strong) NSArray *subVcArray;
 @end
 
 @implementation ViewController
-
+static NSString * const oneCellID = @"oneCellID";
+#pragma mark - 生命周期
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setNav];
+    [self addView];
+    [self regisCell];
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+#pragma mark - 初始化方法
+- (void)setNav
+{
+    self.navigationItem.title = @"iOS_Animations_by_Tutorials";
 }
+- (void)addView
+{
+    [self.view addSubview:({
+        self.rootTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        self.rootTableView.backgroundColor = GPBGColor;
+        self.rootTableView.delegate = self;
+        self.rootTableView.dataSource = self;
+        self.rootTableView.rowHeight = SCREEN_HEIGHT * 0.12;
+        self.rootTableView.tableFooterView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 1)];
+        self.rootTableView;
+    })];
+}
+- (void)regisCell
+{
+    [self.rootTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:oneCellID];
+}
+#pragma mark - 表格数据源
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.titleArray.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *oneCell = [tableView dequeueReusableCellWithIdentifier:oneCellID];
+    oneCell.backgroundColor = GPBGColor;
+    oneCell.textLabel.text = self.titleArray[indexPath.row];
+    return oneCell;
+}
+#pragma mark - 表格代理
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIViewController *tempVc =  SBVC(self.subVcArray[indexPath.row]);
+    tempVc.navTitle = self.titleArray[indexPath.row];
+    [self.navigationController pushViewController:tempVc animated:YES];
+}
+#pragma mark - 懒加载
+- (NSArray *)titleArray
+{
+    if (!_titleArray) {
+        _titleArray = @[];
+    }
+    return _titleArray;
+}
+#pragma mark - 懒加载
+- (NSArray *)subVcArray
+{
+    if (!_subVcArray) {
+        _subVcArray = @[];
+    }
+    return _subVcArray;
+}
+
+
 @end
