f3b680f25c439fe3f7796b97535eb3e8f9464521
DanDan <821403986@qq.com>
2017-04-05 10:36:29 +0800

    02-走你

--- GPAnimationLearning/GPAnimationLearning/Class/GPAirLoginViewController.m
+++ GPAnimationLearning/GPAnimationLearning/Class/GPAirLoginViewController.m
@@ -9,16 +9,25 @@
 #import "GPAirLoginViewController.h"
 
 @interface GPAirLoginViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *topImageView;
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *flightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *FlightNO;
+@property (weak, nonatomic) IBOutlet UILabel *boardingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *boardingNOLabel;
+@property (weak, nonatomic) IBOutlet UILabel *originLabel;
+@property (weak, nonatomic) IBOutlet UILabel *arrivalPointLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *airImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *statusImageView;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @end
 
 @implementation GPAirLoginViewController
+
 #pragma mark - 生命周期
 - (void)viewDidLoad {
     [super viewDidLoad];
     
 }
-
-
 
 @end
