be0ffaff9ab38e0a6746c3a64172cc467b79e5c4
DanDan <821403986@qq.com>
2017-04-05 10:57:16 +0800

    创建数据

---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPFlighData.m
@@ -0,0 +1,45 @@
+//
+//  GPFlighData.m
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/4/5.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import "GPFlighData.h"
+
+@interface GPFlighData()
+@property (nonatomic, strong) NSArray *flightArrray;
+@end
+
+@implementation GPFlighData
+- (instancetype)initWithIsBeiJing:(BOOL)IsBeiJing
+{
+    if (self = [super init]) {
+        [self getFlighData:IsBeiJing];
+    }
+    return self;
+}
+
+- (void)getFlighData:(BOOL)IsBeiJing
+{
+    NSArray *tempDataArray = self.flightArrray[IsBeiJing];
+    self.summaryStr = tempDataArray[0];
+    self.flightNrStr = tempDataArray[1];
+    self.gateNrStr = tempDataArray[2];
+    self.departingFromStr = tempDataArray[3];
+    self.arrivingToStr = tempDataArray[4];
+    self.weatherImageNameStr = tempDataArray[5];
+    self.showWeatherEffects = [tempDataArray[6] integerValue];
+    self.TakingOff = [tempDataArray[7] integerValue];
+    self.flightStatus = tempDataArray[8];
+}
+#pragma mark - 懒加载
+- (NSArray *)flightArrray
+{
+    if (!_flightArrray) {
+        _flightArrray = @[@[@"2017 04 10:47",@"NH 2017",@"T1 A33",@"北京",@"上海",@"bg-snowy",@"1",@"1",@"飞行"],@[@"2017 04 18:40",@"AE 1107",@"045",@"上海",@"哈尔滨",@"bg-sunny",@"0",@"0",@"延迟"]];
+    }
+    return _flightArrray;
+}
+@end
