a80f8b394b22337b4375a4b4dfaf6e34543cdf43
DanDan <821403986@qq.com>
2017-04-18 15:04:51 +0800

    07-初始化

---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPAvatarView.h
@@ -0,0 +1,13 @@
+//
+//  GPAvatarView.h
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/4/18.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPAvatarView : UIView
+
+@end
---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPAvatarView.m
@@ -0,0 +1,16 @@
+//
+//  GPAvatarView.m
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/4/18.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import "GPAvatarView.h"
+
+@implementation GPAvatarView
+
+
+
+
+@end
