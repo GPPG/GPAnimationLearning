b9a1f306cf414f6dfcf71137a6de0cd23d885e1a
DanDan <821403986@qq.com>
2017-04-20 16:36:52 +0800

    jiehzelai

--- GPAnimationLearning/GPAnimationLearning/Class/GPAvatarView.m
+++ GPAnimationLearning/GPAnimationLearning/Class/GPAvatarView.m
@@ -8,9 +8,39 @@
 
 #import "GPAvatarView.h"
 
+IB_DESIGNABLE
+
+#define LineWidth 6
+#define AnimationDuration 1
+
+@interface GPAvatarView()
+@property (nonatomic, copy) IBInspectable NSString *name;
+@property (nonatomic, strong) IBInspectable UIImage *image;
+@property (nonatomic, strong) CALayer *photoLayer;
+@property (nonatomic, strong) CAShapeLayer *circleLayer;
+@property (nonatomic, strong) CAShapeLayer *maskLayer;
+
+@end
+
 @implementation GPAvatarView
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+        [self addView];
+    }
+    return self;
+}
 
-
-
-
+- (void)addView
+{
+    
+}
+#pragma mark - set
+- (void)setName:(NSString *)name
+{
+    if (name.length) {
+        <#statements#>
+    }
+}
 @end
