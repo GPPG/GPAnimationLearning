4a2b833f5818b10cb7a1316d9ea322667c9b1ef9
DanDan <821403986@qq.com>
2017-05-17 10:12:49 +0800

    09-初始化

---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPAnimatedMaskLabelView.m
@@ -0,0 +1,16 @@
+//
+//  GPAnimatedMaskLabelView.m
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/5/16.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import "GPAnimatedMaskLabelView.h"
+
+@implementation GPAnimatedMaskLabelView
+
+
+
+
+@end
---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPGrabienAnimationViewController.m
@@ -0,0 +1,21 @@
+//
+//  GPGrabienAnimationViewController.m
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/5/16.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import "GPGrabienAnimationViewController.h"
+#import "GPAnimatedMaskLabelView.h"
+
+@interface GPGrabienAnimationViewController()
+@property (weak, nonatomic) IBOutlet GPAnimatedMaskLabelView *animatedView;
+@end
+
+
+@implementation GPGrabienAnimationViewController
+
+
+
+@end
