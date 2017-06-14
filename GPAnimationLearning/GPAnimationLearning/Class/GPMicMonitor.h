ddfa0f65ba6974f284e0895e69261444dc645883
DanDan <821403986@qq.com>
2017-06-14 16:06:30 +0800

    10-10
    10-10

---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPMicMonitor.h
@@ -0,0 +1,13 @@
+//
+//  GPMicMonitor.h
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/6/14.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPMicMonitor : NSObject
+
+@end
---
+++ GPAnimationLearning/GPAnimationLearning/Class/GPMicMonitor.m
@@ -0,0 +1,13 @@
+//
+//  GPMicMonitor.m
+//  GPAnimationLearning
+//
+//  Created by 郭鹏 on 2017/6/14.
+//  Copyright © 2017年 郭鹏. All rights reserved.
+//
+
+#import "GPMicMonitor.h"
+
+@implementation GPMicMonitor
+
+@end
